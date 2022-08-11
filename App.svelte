63d839ae96830ad29a77545741fcaa00d5a70c13
Andreas Rozek <A.Rozek@gmx.de>
2022-08-11 06:24:38 +0200

    Create App.svelte

---
+++ App.svelte
@@ -0,0 +1,25 @@
+<script context="module">
+  import "aframe"
+
+  import "./aframe-component.js"
+  import "./aframe-system.js"
+  import "./aframe-primitive.js"
+
+  import AFrameEntity from './AFrameEntity.svelte'
+</script>
+
+<script>
+  let Angle = 45
+  setInterval(() => { Angle++ }, 100)
+</script>
+
+<a-scene a-svelte-system>
+  <a-sky color="#ECECEC"/>
+
+  <AFrameEntity position="0 0.5 -3" rotation="0 {Angle} 0" Hue="180"/>
+  <a-svelte-primitive a-svelte-component a-svelte-system/>
+
+  <a-plane width="4" height="4" color="#7BC8A4"
+    position="0 0 -4" rotation="-90 0 0"
+  />
+</a-scene>
